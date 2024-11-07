2b2f3100c54b5d12b7353db1e8876210dcdd7495
Ti <13039524+yang-mingyong@user.noreply.gitee.com>
2024-11-07 13:24:13 +0800

    vue/2024-11-7/ChildComponent.vue vue/2024-11-7/ParentComponent.vue

---
+++ vue/2024-11-7/ChildComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>{{title}}</h2>
+    <h2>{{description}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChildComponent',
+// 方法一：
+//   props: {
+//     title: {
+//       type: String,
+//       required: true
+//     },
+//     description: {
+//       type: String,
+//       required: true
+//     }
+//   }
+// 方法二：
+  props: ['title', 'description']
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/2024-11-7/ParentComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+     <ChildComponent :title="title" :description="description" />
+  </div>
+</template>
+
+<script>
+import ChildComponent from './components/ChildComponent.vue'
+export default {
+  data() {
+    return {
+      title: "这是标题title属性的内容",
+      description: "这是对应的description属性"
+    }
+  }
+  ,
+  components: {
+    ChildComponent
+  }
+}
+</script>
